7045350758a7ccebc004a47f97064ccbd9ada382
Pierce <p.provias@outlook.com>
2024-05-23 20:18:35 -0400

    Fixed sign up bug

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", type: :request do
+describe "Pages", type: :request do
   describe "GET /home" do
     it "returns http success" do
       get "/pages/home"
