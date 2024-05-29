bc2e11922ca20893cba575430cc28bebed612794
Pierce <p.provias@outlook.com>
2024-05-29 17:10:22 -0400

    Fixed bug for signing in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
 
   
   def index
-    @tasks = Task.where(user: Current.user)
+    @tasks = Task.where(user: Current.user).order(created_at: :desc)
   end
 
   def show
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
     if @task.user
       respond_to do |format|
         if @task.save
-          format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+          format.html { redirect_to tasks_path, notice: "Task was successfully created." }
           format.json { render :show, status: :created, location: @task }
         else
           format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
     @task.destroy!
 
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_url, notice: "Task was successfully completed" }
       format.json { head :no_content }
     end
   end
